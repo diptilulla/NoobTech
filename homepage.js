c054be6536202c227d34b9a23e4408cac1aa39f9
DIPTI LULLA <60360333+diptilulla@users.noreply.github.com>
2020-11-26 01:39:23 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,17 @@
+var button=document.getElementById("getStarted");
+    button.addEventListener("click", function () {
+        window.location.href="signin.html"; }   );
+    var menu=document.getElementById("hamburger");
+    var submenu=document.getElementById("submenu");
+    menu.addEventListener("click", function () {
+        submenu.classList.toggle("display");
+    } );
+    window.addEventListener("resize", function(){
+        var width = document.documentElement.clientWidth;
+    // var height = document.documentElement.clientHeight;
+    if(width>550&&submenu.classList.contains("display"))
+    {
+        document.getElementById("submenu").classList.remove("display");
+    }        
+    });
+
